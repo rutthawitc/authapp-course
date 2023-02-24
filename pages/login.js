461c0c1f0880e136677de1e7a7d63cd9e7677c06
rutthawit ch <rutthawit@gmail.com>
2023-02-24 15:21:46 +0700

    -login style

---
+++ pages/login.js
@@ -0,0 +1,82 @@
+import Head from "next/head";
+import Layout from "../layout/layout";
+import Link from "next/link";
+import style from "../styles/Form.module.css";
+import Image from "next/image";
+import { HiAtSymbol, HiFingerPrint } from "react-icons/hi";
+import { useState } from "react";
+
+function Login() {
+  const [show, setShow] = useState(false);
+  return (
+    <Layout>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <section className="w-3/4 mx-auto flex flex-col gap-10">
+        <div className="title">
+          <h1 className="text-gray-800 text-4xl font-bold py-4">PWA 6</h1>
+          <p className="w-3/4 mx-auto text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
+            officia?
+          </p>
+        </div>
+        {/* form */}
+        <form className="flex flex-col gap-5">
+          <div className={style.input_group}>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className={style.input_text}
+            />
+            <span className="icon flex items-center px-4">
+              <HiAtSymbol size={25} />
+            </span>
+          </div>
+          <div className={style.input_group}>
+            <input
+              type={`${show ? "text" : "password"}`}
+              name="password"
+              placeholder="Password"
+              className={style.input_text}
+            />
+            <span
+              className="icon flex items-center px-4"
+              onClick={() => setShow(!show)}
+            >
+              <HiFingerPrint size={25} />
+            </span>
+          </div>
+          {/* login Button */}
+          <div className="input-button text-gray-900">
+            <button type="submit" className={style.button}>
+              Login
+            </button>
+          </div>
+          <div className="input-button text-gray-900">
+            <button type="button" className={style.button_custom}>
+              Sign In with Google{" "}
+              <Image src="/assets/google.svg" width={20} height={20}></Image>
+            </button>
+          </div>
+          <div className="input-button text-gray-900">
+            <button type="button" className={style.button_custom}>
+              Sign In with Github{" "}
+              <Image src="/assets/github.svg" width={25} height={25}></Image>
+            </button>
+          </div>
+        </form>
+        {/* bottom */}
+        <p className="text-center text-gray-500">
+          Don't have account yet?{" "}
+          <Link href="/register" className="text-blue-700">
+            Sign Up
+          </Link>
+        </p>
+      </section>
+    </Layout>
+  );
+}
+
+export default Login;
---
+++ pages/register.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+function Register() {
+  return (
+    <div>
+      <Head>
+        <title>Register</title>
+      </Head>
+      <h1>Register</h1>
+    </div>
+  );
+}
+
+export default Register;
